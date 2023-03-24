cf27da8294e182e2d4fd1aa35109ef421f27ebde
Jordi Bruin <jordibruin@gmail.com>
2023-03-24 21:55:41 +0100

    Update Whisper.swift

--- Sources/Whisper/Whisper.swift
+++ Sources/Whisper/Whisper.swift
@@ -28,7 +28,7 @@ public class WhisperParams {
         
         let maxThreads = max(1, min(8, ProcessInfo.processInfo.processorCount - 2))
         self.n_threads = Int32(maxThreads)
-        self.ml = 1
+        self.max-len = 1
     }
 
     deinit {
